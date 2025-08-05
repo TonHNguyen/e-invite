09ec1e4e801b47b92fe277c1dcc986892cf7c1d1
noname1112392 <149623681+noname1112392@users.noreply.github.com>
2025-08-04 17:50:14 -0700

    Added tailwind files

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # e-invite
+# this is a e-invite for Xoai first birthday. 
+# in this project we will create a template with the purpose of it.
+# will have 2 check boxes: accepting and declining
+# will have a submit button to submit the form
+# the end-user 'me" will get email to update of people who go and total of number of people.
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {},
+    screens: {
+      'sm': '414px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1920px',
+      '2xl': '2560px',
+      '3xl': '3840px'
+    }
+  },
+  plugins: ['@tailwindcss/filters'],
+}
+
